94f1607a51655dcc700538818807d8000cf3eae4
Pete Wildsmith <pete@dabapps.com>
2016-06-20 19:25:02 +0100

    superfast buffers yeah man

--- index.js
+++ index.js
@@ -1,29 +1,42 @@
 import THREE from 'three';
+import {BTMap} from 'ibtree';
+var createTree = require('yaot');
 var fs = require('fs');
+var interleave = require('bit-interleave');
 
 
 let camera, scene, renderer;
 
-let displacements, velocities, orientations;
+let displacement_buffer, displacements, velocities, orientations;
 
 let max_velocity = 1;
 let boid_mass = 1;
-let boid_force = 0.05;
+let boid_force = 0.15;
+
+let sorted, tree, neighbours, neighbourhood = 0.3;
+
+
+const GOAL_WEIGHT = 2;
+const SEPARATION_WEIGHT = 2.8;
+const HEADING_WEIGHT = 2;
+const COHESION_WEIGHT = 3;
+
+let gravity = new THREE.Vector3(0.0, 0.0, 0.0);
 
 init();
 
 
-function init () {
+function init() {
     let body = document.getElementsByTagName('body')[0];
     body.style.margin = 0;
-    camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 10);
-    camera.position.z = 2;
+    camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 50);
+    camera.position.z = 5;
 
     scene = new THREE.Scene();
     renderer = new THREE.WebGLRenderer();
 
     const triangles = 4;
-    const instances = 2500;
+    const instances = 1000;
 
     const geometry = new THREE.InstancedBufferGeometry();
 
@@ -70,19 +83,27 @@ function init () {
 
     geometry.addAttribute('normal', normals);
 
-    displacements = new THREE.InstancedBufferAttribute(new Float32Array(instances * 3), 3, 1);
+    displacement_buffer = new Float32Array(instances * 3);
+    displacements = new THREE.InstancedBufferAttribute(displacement_buffer, 3, 1);
 
     let vector = new THREE.Vector3();
     for (let i = 0, ul = displacements.count; i < ul; i++) {
         vector.set(
-            Math.random() * 10 - 5,
-            Math.random() * 10 - 5,
-            Math.random() * 10 - 5
-        ).normalize();
+            Math.random() * 5 - 5,
+            Math.random() * 5 - 5,
+            Math.random() * 5 - 5
+        );
         displacements.setXYZ(i, vector.x, vector.y, vector.z);
     }
 
     geometry.addAttribute('displacement', displacements);
+
+    tree = new BTMap();
+    for (let i = 0, j = 0, k = 0, ul = displacements.count; i < ul; i++) {
+        j = i * 3;
+        k = interleave_from_vector(displacement_buffer[j], displacement_buffer[j + 1], displacement_buffer[j + 2]);
+        tree = tree.set(k, i);
+    }
 
     velocities = new THREE.InstancedBufferAttribute(new Float32Array(instances * 3), 3, 1);
 
@@ -113,32 +134,32 @@ function init () {
 
     let light = new THREE.Vector3(350.0, 350.0, 350.0);
     // light.normalize();
-    var material = new THREE.RawShaderMaterial( {
+    var material = new THREE.RawShaderMaterial({
         uniforms: {
-            light: { type: 'v3', value: light }
+            light: {type: 'v3', value: light}
         },
         vertexShader: fs.readFileSync(__dirname + '/shaders/vertex.glsl', 'utf8'),
         fragmentShader: fs.readFileSync(__dirname + '/shaders/fragment.glsl', 'utf8'),
         side: THREE.FrontSide,
         transparent: false
-    } );
+    });
 
     var mesh = new THREE.Mesh(geometry, material);
     scene.add(mesh);
 
 
-    renderer.setClearColor( 0xa0c0f0 );
-    renderer.setPixelRatio( window.devicePixelRatio );
-    renderer.setSize( window.innerWidth - 16, window.innerHeight - 16);
-    body.appendChild( renderer.domElement );
-    window.addEventListener( 'resize', onWindowResize, false );
+    renderer.setClearColor(0xa0c0f0);
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth - 16, window.innerHeight - 16);
+    body.appendChild(renderer.domElement);
+    window.addEventListener('resize', onWindowResize, false);
 }
 
 
-function onWindowResize( event ) {
+function onWindowResize(event) {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
-    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
 
@@ -147,7 +168,7 @@ function animate(now) {
     render(now);
     requestAnimationFrame(animate);
 }
-let then = performance.now();
+let then = performance.now(), last_octree_update = performance.now();
 
 let currentV = new THREE.Vector3();
 let currentVel = new THREE.Vector3();
@@ -156,13 +177,30 @@ let target = new THREE.Vector3(0, 0, 0);
 let neg_loc = new THREE.Vector3();
 let neg_vel = new THREE.Vector3();
 let v_tmp = new THREE.Vector3();
+let tmp_1 = new THREE.Vector3();
+let tmp_2 = new THREE.Vector3();
 let goal_steering_force = new THREE.Vector3();
 
-function render (now) {
+let separation = new THREE.Vector3(),
+    heading = new THREE.Vector3(),
+    cohesion = new THREE.Vector3(),
+    neighbour_position = new THREE.Vector3(),
+    neighbour_velocity = new THREE.Vector3();
+
+let lo = new THREE.Vector3(), hi = new THREE.Vector3(), neighbourhood_v = new THREE.Vector3(1, 1, 1).normalize().multiplyScalar(neighbourhood);
+
+function interleave_from_vector(x, y, z) {
+    return interleave[3](x << 9, y << 9, z << 9);
+}
+
+function render(now) {
     renderer.render(scene, camera);
     let dt = (now - then) / 1000;
     then = now;
     if (isNaN(dt)) {
+        return;
+    }
+    if (dt > 0.2) {
         return;
     }
     //console.log(dt);
@@ -184,8 +222,92 @@ function render (now) {
         goal_steering_force.add(neg_vel);
         goal_steering_force.normalize();
 
+        // do neighbour calculations here
+        //neighbours = octree.intersectSphere(currentV.x, currentV.y, currentV.z, neighbourhood);
+        lo.copy(currentV);
+        lo.addScaledVector(neighbourhood_v, -1);
+        hi.copy(currentV);
+        hi.add(neighbourhood_v);
+        // console.log(lo, hi);
+
+        neighbours = Array.from(tree.values(
+            interleave_from_vector(lo.x, lo.y, lo.z),
+            interleave_from_vector(hi.x, hi.y, hi.z)
+        ));
+        if (neighbours.length) {
+            // console.log("neighbours:", neighbours);
+            separation.set(0, 0, 0);
+            heading.set(0, 0, 0);
+            cohesion.set(0, 0, 0);
+            for (let j = 0, nl = neighbours.length, ni; j < nl; j++) {
+                ni = neighbours[j] * 3;
+                neighbour_position.set(displacement_buffer[ni], displacement_buffer[ni + 1], displacement_buffer[ni + 2]);
+                neighbour_velocity.set(displacement_buffer[ni], displacement_buffer[ni + 1], displacement_buffer[ni + 2])
+                tmp_1.copy(neg_loc).add(neighbour_position); // neighbour displacement
+                separation.add(tmp_1);
+
+                neighbour_velocity.normalize();
+                heading.add(tmp_1);
+
+                cohesion.add(neighbour_position);
+            }
+            separation.multiplyScalar(-1).normalize();
+            heading.normalize();
+            cohesion.multiplyScalar(1/neighbours.length);
+            cohesion.add(neg_loc);
+            cohesion.normalize();
+
+            /*
+             if (neighbours.length) {
+             mul(separation, 0, separation);
+             mul(heading, 0, heading);
+             mul(cohesion, 0, cohesion);
+             for (let i = 0; i < neighbours.length ; i++) {
+             neighbour_offset = neighbours[i].boid;
+
+             neighbour_position = boids[neighbour_offset][0];
+             neighbour_velocity = boids[neighbour_offset][1];
+             // logVector("np", neighbour_position);
+             // logVector("nv", neighbour_velocity);
+             // console.log('---');
+             add(neg_loc, neighbour_position, tmp1);
+             add(separation, tmp1, separation);
+
+             normalize(neighbour_velocity, tmp1);
+             add(heading, tmp1, heading);
+
+             add(cohesion, neighbour_position, cohesion);
+             }
+             mul(separation, -1, separation);
+             normalize(separation, separation);
+             normalize(heading, heading);
+             mul(cohesion, 1/neighbours.length, cohesion);
+             add(cohesion, neg_loc, cohesion);
+             normalize(cohesion, cohesion);
+             */
+            goal_steering_force.multiplyScalar(GOAL_WEIGHT);
+            goal_steering_force.addScaledVector(separation, SEPARATION_WEIGHT);
+            goal_steering_force.addScaledVector(heading, HEADING_WEIGHT);
+            goal_steering_force.addScaledVector(cohesion, COHESION_WEIGHT);
+
+            goal_steering_force.normalize();
+
+            /*
+             mul(goal_steering_force, 3, goal_steering_force);
+             mul(separation, 2.5, separation);
+             add(goal_steering_force, separation, goal_steering_force);
+             mul(heading, 2, heading);
+             add(goal_steering_force, heading, goal_steering_force);
+             mul(cohesion, 3, cohesion);
+             add(goal_steering_force, cohesion, goal_steering_force);
+             normalize(goal_steering_force, goal_steering_force);
+             */
+        }
+        // end neighbour calculations
+
         goal_steering_force.multiplyScalar(boid_force);
-        goal_steering_force.multiplyScalar(dt/boid_mass);
+        goal_steering_force.multiplyScalar(dt / boid_mass);
+        goal_steering_force.addScaledVector(gravity, dt);
         //console.log(goal_steering_force);
 
         currentVel.add(goal_steering_force);
@@ -199,22 +321,33 @@ function render (now) {
         //currentO.set(orientations.array[index], orientations.array[index + 1], orientations.array[index + 2]);
         v_tmp.copy(currentVel);
         v_tmp.normalize();
-        currentO.setFromAxisAngle(v_tmp, Math.PI / 2);
+        tmp_2.set(0.0, 0.0, 1.0);
+        // tmp_2.normalize();
+        v_tmp.add(tmp_2);
+        v_tmp.normalize();
+        currentO.setFromAxisAngle(v_tmp, Math.PI);
         orientations.setXYZW(i, currentO.x, currentO.y, currentO.z, currentO.w);
         /*
-        mul(position, -1, neg_loc);
-        mul(velocity, -1, neg_vel);
-        // console.log(neg_loc, neg_vel);
-        add(target, neg_loc, goal_steering_force);
-        normalize(goal_steering_force, goal_steering_force);
-        mul(goal_steering_force, max_boid_velocity, goal_steering_force);
-        add(goal_steering_force, neg_vel, goal_steering_force);
-        normalize(goal_steering_force, goal_steering_force);
-        */
+         mul(position, -1, neg_loc);
+         mul(velocity, -1, neg_vel);
+         // console.log(neg_loc, neg_vel);
+         add(target, neg_loc, goal_steering_force);
+         normalize(goal_steering_force, goal_steering_force);
+         mul(goal_steering_force, max_boid_velocity, goal_steering_force);
+         add(goal_steering_force, neg_vel, goal_steering_force);
+         normalize(goal_steering_force, goal_steering_force);
+         */
     }
     displacements.needsUpdate = true;
     velocities.needsUpdate = true;
     orientations.needsUpdate = true;
+
+    tree = new BTMap();
+    for (let i = 0, j = 0, k = 0, ul = displacements.count; i < ul; i++) {
+        j = i * 3;
+        k = interleave_from_vector(displacement_buffer[j], displacement_buffer[j + 1], displacement_buffer[j + 2]);
+        tree = tree.set(k, i);
+    }
 }
 
 requestAnimationFrame(animate);
--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -12,10 +12,15 @@ attribute vec4 orientation;
 varying vec3 vPosition;
 varying vec3 v_ecNormal;
 
+vec3 qtransform( vec4 q, vec3 v ){
+	return v + 2.0*cross(cross(v, q.xyz ) + q.w*v, q.xyz);
+}
+
 void main(){
-    vPosition = position;
-    vec3 vcV = cross(orientation.xyz, vPosition);
-    vPosition = vcV * (2.0 * orientation.w) + (cross(orientation.xyz, vcV) * 2.0 + vPosition);
+//    vPosition = position;
+//    vec3 vcV = cross(orientation.xyz, vPosition);
+//    vPosition = vcV * (2.0 * orientation.w) + (cross(orientation.xyz, vcV) * 2.0 + vPosition);
+    vPosition = qtransform(orientation, position);
 
     gl_Position = projectionMatrix * modelViewMatrix * vec4(displacement + vPosition, 1.0 );
 
